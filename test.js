db720b2be71133b8c05539406897ad61f5520c33
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-18 17:29:45 +0000

    Require Node.js 6, add TypeScript definition (#1)

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 import test from 'ava';
 import delay from 'delay';
-import m from './';
+import pRace from '.';
 
 test('works like Promise.race()', async t => {
-	t.is(await m([delay(50).then(() => 1), delay(100).then(() => 2)]), 1);
+	t.is(await pRace([delay(50, {value: 1}), delay(100, {value: 2})]), 1);
 });
 
 test('handles empty Iterable', async t => {
-	await t.throws(m([]), RangeError);
+	await t.throwsAsync(pRace([]), RangeError);
 });
