adff9ed840adbdcd4990693de06c2cc1b363f2c8
Gyubong <jopemachine@naver.com>
2022-12-13 20:55:28 +0900

    Fix broken CI issue on Node 12

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const toIterable = (executorOrIterable, signal) => {
 export default async function pRace(executorOrIterable) {
 	const abortController = globalThis.AbortController ? new AbortController() : undefined;
 
-	const iterable = toIterable(executorOrIterable, abortController?.signal);
+	const iterable = toIterable(executorOrIterable, abortController ? abortController.signal : undefined);
 
 	if (isEmptyIterable(iterable)) {
 		throw new RangeError('Expected the iterable to contain at least one item');
 	}
 
 	const result = await Promise.race(iterable);
-	abortController?.abort();
+
+	if (abortController) {
+		abortController.abort();
+	}
+
 	return result;
 }
